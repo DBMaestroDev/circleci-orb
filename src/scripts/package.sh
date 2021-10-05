bf965ef68aaa6da2807e59f0fdf8b9d04aff1d3f
Olekasndr Kosenko <sashao@dbmaestro.com>
2021-10-05 14:59:39 +0300

    variables

--- src/scripts/package.sh
+++ src/scripts/package.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 Package(){
-    if [ -z "${DBM_TOOL_PATH:=${DBMAESTRO_TOOL_PATH:-'tools/dbmaestro'}}" ]; then echo "DBmaestro tool path is not set"&&exit 1; fi
-    if [ -z "${DBM_SERVER_ADDRESS:=$DBMAESTRO_SERVER_ADDRESS}" ]; then echo "DBmaestro server address is not set"&&exit 1; fi
-    if [ -z "${DBM_USE_SSL:=${DBMAESTRO_USE_SSL:-n}}" ]; then echo "DBmaestro SSL usage is not set"&&exit 1; fi
-    if [ -z "${DBM_AUTH_TYPE:=${DBMAESTRO_AUTH_TYPE:-DBmaestroAccount}}" ]; then echo "DBmaestro auth type is not set"&&exit 1; fi
-    if [ -z "${DBM_USERNAME:=$DBMAESTRO_USERNAME}" ]; then echo "DBmaestro username is not set"&&exit 1; fi
+    if [ -z "${DBM_TOOL_PATH:=${!PDBM_TOOL_PATH:-'tools/dbmaestro'}}" ]; then echo "DBmaestro tool path is not set"&&exit 1; fi
+    if [ -z "${DBM_SERVER_ADDRESS:=${!PDBM_SERVER_ADDRESS}}" ]; then echo "DBmaestro server address is not set"&&exit 1; fi
+    if [ -z "${DBM_USE_SSL:=${!PDBM_USE_SSL:-n}}" ]; then echo "DBmaestro SSL usage is not set"&&exit 1; fi
+    if [ -z "${DBM_AUTH_TYPE:=${!PDBM_AUTH_TYPE:-DBmaestroAccount}}" ]; then echo "DBmaestro auth type is not set"&&exit 1; fi
+    if [ -z "${DBM_USERNAME:=${!PDBM_USERNAME}}" ]; then echo "DBmaestro username is not set"&&exit 1; fi
     if [ -z "${DBM_PWD:=${!DBM_PASSWORD}}" ]; then echo "DBMAESTRO_PASSWORD variable is not set"&&exit 1; fi
     CMDLINE='java -jar "'$DBM_TOOL_PATH'/DBmaestroAgent.jar" -Package -ProjectName "'$DBM_PROJECT_NAME'" '
     if [[  -n ${DBM_IGNORE_SCRIPT_WARNINGS} ]]; then
